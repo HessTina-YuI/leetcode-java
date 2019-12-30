e01bef05c191aee602a74294e792df5779ee207b
YuI <HessTinaYuI@Gmail.com>
2019-12-31 00:23:32 +0800

    增加 leetcode第28题

---
+++ src/main/java/org/yui/easy/p0028/Solution.java
@@ -0,0 +1,20 @@
+package org.yui.easy.p0028;
+
+/**
+ * @program: leet-code
+ * @description: 28.实现strStr()
+ * @author: yui
+ * @create: 2019-12-30 21:49
+ **/
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.strStr("hello", "");
+
+        System.out.println(result);
+    }
+}
