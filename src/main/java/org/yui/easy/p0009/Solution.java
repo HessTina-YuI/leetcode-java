253746e721ef774b1d742616f044f77c11689f26
YuI <HessTinaYuI@Gmail.com>
2019-12-31 00:19:17 +0800

    增加 leetcode第9题

---
+++ src/main/java/org/yui/easy/p0009/Solution.java
@@ -0,0 +1,31 @@
+package org.yui.easy.p0009;
+
+/**
+ * @program: leet-code
+ * @description: 9.回文数
+ * @author: yui
+ * @create: 2019-12-30 18:09
+ **/
+public class Solution {
+
+    public boolean isPalindrome(int x) {
+        char[] chars = String.valueOf(x).toCharArray();
+        int length = chars.length;
+
+        for (int i = 0; i <= length / 2; i++) {
+            if (chars[i] != chars[length - 1 - i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        boolean result = solution.isPalindrome(10);
+
+        System.out.println(result);
+    }
+
+}
