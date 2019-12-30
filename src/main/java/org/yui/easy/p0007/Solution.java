3d314a6bbd47274baec4eac9556996199f0abd50
YuI <HessTinaYuI@Gmail.com>
2019-12-31 00:18:49 +0800

    增加 leetcode第7题

---
+++ src/main/java/org/yui/easy/p0007/Solution.java
@@ -0,0 +1,29 @@
+package org.yui.easy.p0007;
+
+/**
+ * @program: leet-code
+ * @description: 7.整数反转
+ * @author: yui
+ * @create: 2019-12-30 17:30
+ **/
+public class Solution {
+
+    public int reverse(int x) {
+        long res = 0;
+
+        for (; x != 0; x /= 10) {
+            res = res * 10 + x % 10;
+        }
+
+        return res > Integer.MAX_VALUE || res < Integer.MIN_VALUE ? 0 : (int) res;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.reverse(-15);
+
+        System.out.println(result);
+    }
+
+}
