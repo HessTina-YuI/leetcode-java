85608c1b8c326ab279c1a4e47d3bf222ceac5044
YuI <HessTinaYuI@Gmail.com>
2020-01-01 15:20:35 +0800

    增加 leetcode第83题

---
+++ src/main/java/org/yui/easy/p0083/Solution.java
@@ -0,0 +1,45 @@
+package org.yui.easy.p0083;
+
+import org.yui.structure.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ *
+ * @program: leetcode
+ * @description: 83.删除排序链表中的重复元素
+ * @author: yui
+ * @create: 2020-01-01 14:39
+ **/
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head;
+        ListNode pre;
+
+        while (cur != null) {
+            int temp = cur.val;
+            pre = cur;
+            cur = cur.next;
+            if (cur == null) {
+                break;
+            }
+            if (temp == cur.val) {
+                pre.next = cur.next;
+                cur = pre;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(new int[]{1, 1, 2, 3, 3});
+        Solution solution = new Solution();
+        ListNode result = solution.deleteDuplicates(head);
+
+        System.out.println(result);
+    }
+}
