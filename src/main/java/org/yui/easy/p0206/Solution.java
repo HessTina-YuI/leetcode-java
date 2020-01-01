a04f7634b2085ecc15519abe6331e0ecdd9f95d9
YuI <HessTinaYuI@Gmail.com>
2020-01-01 18:48:11 +0800

    增加 leetcode第206题

---
+++ src/main/java/org/yui/easy/p0206/Solution.java
@@ -0,0 +1,40 @@
+package org.yui.easy.p0206;
+
+import org.yui.structure.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ *
+ * @program: leetcode
+ * @description: 206.反转链表
+ * @author: yui
+ * @create: 2020-01-01 18:37
+ **/
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        ListNode temp;
+        while (cur != null) {
+            temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(new int[]{1,2,3,4,5});
+        Solution solution = new Solution();
+        ListNode result = solution.reverseList(head);
+
+        System.out.println(result);
+    }
+}
