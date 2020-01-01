ba29e3a35d50c361932669d5ea56be20a9a4bb5a
YuI <HessTinaYuI@Gmail.com>
2020-01-02 03:44:07 +0800

    增加 leetcode第237题

---
+++ src/main/java/org/yui/easy/p0237/Solution.java
@@ -0,0 +1,36 @@
+package org.yui.easy.p0237;
+
+import org.yui.structure.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ *
+ * @program: leetcode
+ * @description: 237.删除链表中的节点
+ * @author: yui
+ * @create: 2020-01-02 03:34
+ **/
+public class Solution {
+    public void deleteNode(ListNode node) {
+        while (node != null && node.next != null) {
+            node.val = node.next.val;
+            if (node.next.next == null) {
+                node.next = null;
+            }
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(new int[]{4,5,1,9});
+        Solution solution = new Solution();
+        solution.deleteNode(head.next);
+
+        System.out.println(head.printNode());
+    }
+}
