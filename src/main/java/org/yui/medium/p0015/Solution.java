11c1d0bc5014c34dbd6160b46eff7ca8d25e03e5
YuI <HessTinaYuI@Gmail.com>
2020-01-03 02:21:45 +0800

    增加 leetcode第15题

---
+++ src/main/java/org/yui/medium/p0015/Solution.java
@@ -0,0 +1,53 @@
+package org.yui.medium.p0015;
+
+import java.util.*;
+
+/**
+ * @program: leetcode
+ * @description: 15.三数之和
+ * @author: yui
+ * @create: 2020-01-03 01:26
+ **/
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if (nums[i] > 0) {
+                break;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0) {
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    result.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k])));
+                    while (j < k && nums[j] == nums[++j]) ;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<List<Integer>> result = solution.threeSum(new int[]{-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0});
+
+        System.out.println(result);
+    }
+
+}
