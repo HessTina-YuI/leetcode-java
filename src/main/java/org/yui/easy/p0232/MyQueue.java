f8db796490ecb2769c202f228f85351cc6ffb419
YuI <HessTinaYuI@Gmail.com>
2020-01-02 20:31:39 +0800

    增加 leetcode第232题

---
+++ src/main/java/org/yui/easy/p0232/MyQueue.java
@@ -0,0 +1,69 @@
+package org.yui.easy.p0232;
+
+import java.util.Stack;
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ *
+ * @program: leetcode
+ * @description: 232.用栈实现队列
+ * @author: yui
+ * @create: 2020-01-02 20:17
+ **/
+public class MyQueue {
+
+    private Stack<Integer> pushStack;
+    private Stack<Integer> popStack;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyQueue() {
+        pushStack = new Stack<>();
+        popStack = new Stack<>();
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        pushStack.push(x);
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        if (popStack.isEmpty()) {
+            while (!pushStack.isEmpty()) {
+                popStack.push(pushStack.pop());
+            }
+        }
+        return popStack.pop();
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        if (popStack.isEmpty()) {
+            while (!pushStack.isEmpty()) {
+                popStack.push(pushStack.pop());
+            }
+        }
+        return popStack.peek();
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        return pushStack.isEmpty() && popStack.isEmpty();
+    }
+
+}
