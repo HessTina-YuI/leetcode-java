3681d759523bc3b31f370b6250aeff90a8381d82
YuI <HessTinaYuI@Gmail.com>
2020-01-02 20:45:09 +0800

    优化 leetcode第20题

--- src/main/java/org/yui/easy/p0020/Solution.java
+++ src/main/java/org/yui/easy/p0020/Solution.java
@@ -1,5 +1,7 @@
 package org.yui.easy.p0020;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -9,10 +11,10 @@ import java.util.Stack;
  * @create: 2019-12-30 20:07
  **/
 public class Solution {
-    public boolean isValid(String s) {
+    public boolean isValid1(String s) {
         char[] chars = s.toCharArray();
 
-        Stack<Character> stack = new Stack<Character>();
+        Stack<Character> stack = new Stack<>();
         for (char c : chars) {
             switch (c) {
                 case ')':
@@ -45,9 +47,28 @@ public class Solution {
         return stack.empty();
     }
 
+    public boolean isValid2(String s) {
+        char[] chars = s.toCharArray();
+
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> maps = new HashMap<>(3);
+        maps.put(')', '(');
+        maps.put(']', '[');
+        maps.put('}', '{');
+        for (char c : chars) {
+            if (!maps.containsKey(c)) {
+                stack.push(c);
+            } else if (stack.isEmpty() || !maps.get(c).equals(stack.pop())) {
+                return false;
+            }
+        }
+
+        return stack.empty();
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        boolean result = solution.isValid("{[]}");
+        boolean result = solution.isValid2("{[()()()]}");
 
         System.out.println(result);
     }
