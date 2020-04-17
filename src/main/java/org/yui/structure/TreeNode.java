24d19ba097541a03a1aaf1edab727f1a6d117802
YuI <474766481@qq.com>
2020-04-17 22:12:38 +0800

    增加 二叉树的POJO类

---
+++ src/main/java/org/yui/structure/TreeNode.java
@@ -0,0 +1,20 @@
+package org.yui.structure;
+
+/**
+ * @program: leetcode
+ * @package: org.yui.structure
+ * @description: 二叉树的POJO类
+ * @author: YuI
+ * @create: 2020/4/17 21:37
+ **/
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+
+}
